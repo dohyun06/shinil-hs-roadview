2690eb22cb9e7133adb89ddaf2982ad880ed5d2d
dohyun06 <hyun4you2006@naver.com>
2022-05-28 14:57:39 +0900

    Prepare for deployment

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-app.use('/public', express.static(__dirname + '/public')); /* pug에서 /public으로 경로가 시작될 경우 public 폴더와 연결 */
+app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (_: any, res: any) => res.render('index.html'));
 app.get('/*', (_: any, res: any) => res.redirect('/'));
 
