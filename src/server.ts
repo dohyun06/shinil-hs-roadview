17834b1a37e975e56e4618e87ef0dc048e69cd9c
dohyun06 <hyun4you2006@naver.com>
2022-05-28 15:02:55 +0900

    fix error

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
@@ -8,4 +10,4 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (_: any, res: any) => res.render('index.html'));
 app.get('/*', (_: any, res: any) => res.redirect('/'));
 
-app.listen(3000, console.log('http://localhost:3000'));
+app.listen(PORT, console.log('http://localhost:3000'));
